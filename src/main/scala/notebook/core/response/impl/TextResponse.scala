6469c9ac026b438ba2c01c3f426865bcb0fd3bbd
fkmt.disk <fkmt.disk@gmail.com>
2013-11-27 22:38:55 +0900

    日本語に対応してなかった。。。

--- src/main/scala/notebook/core/response/impl/TextResponse.scala
+++ src/main/scala/notebook/core/response/impl/TextResponse.scala
@@ -7,9 +7,9 @@ import notebook.core.response.ContentType
 
 private[core] case class TextResponse(code: Int, content: String)(implicit exchg: HttpExchange) extends Response(code) {
   
-  val contentType = ContentType.Html.contentType
+  val contentType = s"${ContentType.Html.contentType};charset=utf-8"
   
-  val contentLength: Long = content.length
+  val contentLength: Long = content.getBytes.length
   
   override def openStream = IOUtils.toInputStream(content)
   
