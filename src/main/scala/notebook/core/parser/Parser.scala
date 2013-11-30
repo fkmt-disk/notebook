a7e23dbb78669faa43a4b8893801c0f674d2b797
fkmt.disk <fkmt.disk@gmail.com>
2013-11-30 14:31:12 +0900

    別記法埋め込みがいろいろ中途半端だったので修正

--- src/main/scala/notebook/core/parser/Parser.scala
+++ src/main/scala/notebook/core/parser/Parser.scala
@@ -4,7 +4,21 @@ import scala.util.control.Exception._
 
 private[core] abstract class Parser {
   
-  def parse(text: String): String = (catching(classOf[Exception]) either parseImpl(text)).fold(l => l.toString, r => r)
+  val pattern = """(?ms)/(\w+)/>>>(.*?)<<<""".r
+  
+  def parse(text: String): String = catching(classOf[Exception]).either {
+    val sb = new StringBuilder
+    
+    val idx = pattern.findAllMatchIn(text).foldLeft(0) { (i, m) =>
+      sb append parseImpl(text.slice(i, m.start))
+      sb append Extension.lookup(s".${m.group(1)}").map( _.parser.parse(m group 2) ).getOrElse(m.matched)
+      m.end
+    }
+    
+    sb append parseImpl(text.substring(idx))
+    
+    sb.toString
+  }.fold(l => l.toString, r => r)
   
   protected def parseImpl(text: String): String
   
